15f732ad52d156aac0ceada25e40743e69c6ccd2
kuujinbo <kuujinbo@gmail.com>
2017-08-18 23:00:20 -0700

    optional parameter

--- src/kuujinbo.ASP.NET.Mvc/HtmlHelpers/Extensions.cs
+++ src/kuujinbo.ASP.NET.Mvc/HtmlHelpers/Extensions.cs
@@ -4,23 +4,23 @@ namespace System.Web.Mvc
 {
     public static class Extensions
     {
-        public static MvcHtmlString SimpleFileUpload(this HtmlHelper helper)
+        public static MvcHtmlString SimpleFileUpload(this HtmlHelper helper, string buttonText = "Browse")
         {
             var html = new StringBuilder("<script type='text/javascript'>", 4096);
             html.AppendLine(kuujinbo.ASP.NET.Mvc.Properties.Resources.SimpleFileUpload);
-            html.AppendLine(
-@"</script>
+            html.AppendFormat(@"
+</script>
 <div class='input-group'>
     <span class='input-group-btn'>
         <label class='btn btn-success' type='button'>
-            <input id='simpleFileUpload' name='simpleFileUpload' type='file' onchange='fileUpload(this)' style='display:none;'>Browse....
+            <input id='simpleFileUpload' name='simpleFileUpload' type='file' onchange='fileUpload(this)' style='display:none;'>{0}....
         </label>
     </span>
     <input disabled type='text' class='form-control'>
     <span class='input-group-btn' style='display:none;'>
         <button class='btn btn-danger' type='button' onclick='clearUpload(this)'><span class='glyphicon glyphicon-remove'></span></button>
     </span>
-</div>");
+</div>", buttonText);
             return new MvcHtmlString(html.ToString());
         }
     }
