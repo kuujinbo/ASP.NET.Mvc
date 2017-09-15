13cd4bac7121efb67c3b0653e1b543a93e50e7e9
kuujinbo <kuujinbo@gmail.com>
2017-09-14 21:15:51 -0700

    initial client && server working example

--- src/kuujinbo.ASP.NET.Mvc.Examples/Scripts/jQueryAutoCompleteTest.js
+++ src/kuujinbo.ASP.NET.Mvc.Examples/Scripts/jQueryAutoCompleteTest.js
@@ -3,32 +3,56 @@ addButton.addEventListener(
     'click',
     function(e) {
         var target = e.target;
-        $.ajax({
-            url: target.dataset.url
-            , data: { users: ["1", "2"] }
-            , type: 'POST'
-            , headers: new Xhr().getXsrfToken(target)
-        })
-        .done(function (data, textStatus, jqXHR) {
-            $('<div></div>').html(data).dialog({ width: 'auto', modal: true, title: 'Users Added' });
-        })
-        .fail(function (jqXHR, textStatus, errorThrown) {
-            alert('Error');
-        })
-        .always(function () {
-            //
-        });
+        var toAdd = userContainer.querySelectorAll('div.badge');
+        if (toAdd && toAdd.length > 0) {
+            var users = [];
+            for (var i = 0; i < toAdd.length; ++i) { users.push(toAdd[i].innerText); }
+            console.log(users);
+
+            $.ajax({
+                url: target.dataset.url
+                // , data: { users: ["1", "2"] }
+                , data: { users: users }
+                , type: 'POST'
+                , headers: new Xhr().getXsrfToken(target)
+            })
+            .done(function (data, textStatus, jqXHR) {
+                $('<div></div>').html(data).dialog({ width: 'auto', modal: true, title: 'Users Added' });
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                alert('Error');
+            })
+            .always(function () {
+                //
+            });
+        }
     }
+    , false
 );
 
 var userContainer = document.querySelector('#user-list').firstElementChild;
-console.log(userContainer);
+userContainer.addEventListener(
+    'click',
+    function (e) {
+        var target = e.target;
+        var tag = target.tagName.toLowerCase();
+        if (tag === 'span') {
+            removeFromDom(target.parentElement)
+        } else if (tag === 'div') {
+            removeFromDom(target)
+        }
+        console.log(target);
+    }
+    , false
+);
+// console.log(userContainer);
 
 function addToDom(id, name, office) {
     if (!userContainer.querySelector('[data-id="' + id + '"]')) {
         var div = document.createElement('div');
-        div.innerHTML = "<div class='badge small' data-id='" + id + "'>"
-            + name + ' - ' + office + '</div>';
+        div.innerHTML = "<div class='badge small progress-bar-success' data-id='" + id + "'>"
+            + name + ' - ' + office
+            + " <span class='glyphicon glyphicon-remove text-danger' style='font-size:1.08em;' role='button'></span></div>";
         userContainer.appendChild(div);
     }
 }
