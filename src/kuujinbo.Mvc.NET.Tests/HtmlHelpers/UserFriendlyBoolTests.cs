4489157d93c954cf48797cfa4c89d00ecda1c41c
kuujinbo <kuujinbo@gmail.com>
2017-10-09 17:35:05 -0700

    UserFriendlyBool user-defined values; udapte tests

---
+++ src/kuujinbo.Mvc.NET.Tests/HtmlHelpers/UserFriendlyBoolTests.cs
@@ -0,0 +1,101 @@
+﻿using kuujinbo.Mvc.NET.HtmlHelpers;
+using Moq;
+using System.Web.Mvc;
+using Xunit;
+
+namespace kuujinbo.Mvc.NET.Tests.HtmlHelpers
+{
+    public class UserFriendlyBoolTests
+    {
+        string altTrue = "当たり前";
+        string altFalse = "Hell NO!";
+
+        [Fact]
+        public void Convert_DefaultContructorParametersTrue_ReturnsUserFriendlyTrue()
+        {
+            var result = new UserFriendlyBool().Convert(true);
+
+            Assert.Equal(UserFriendlyBool.UserFriendlyTrue, result.ToString());
+        }
+
+        [Fact]
+        public void Convert_DefaultContructorParametersFalse_ReturnsUserFriendlyFalse()
+        {
+            var result = new UserFriendlyBool().Convert(false);
+
+            Assert.Equal(UserFriendlyBool.UserFriendlyFalse, result.ToString());
+        }
+
+        [Fact]
+        public void Convert_DefaultContructorParametersNull_ReturnsUserFriendlyFalse()
+        {
+            bool? nullBool = null;
+            var result = new UserFriendlyBool().Convert(nullBool);
+
+            Assert.Equal(UserFriendlyBool.UserFriendlyFalse, result.ToString());
+        }
+
+        [Fact]
+        public void Convert_DefaultContructorParametersNullableTrue_ReturnsUserFriendlyTrue()
+        {
+            bool? nullBool = true;
+            var result = new UserFriendlyBool().Convert(nullBool);
+
+            Assert.Equal(UserFriendlyBool.UserFriendlyTrue, result.ToString());
+        }
+
+        [Fact]
+        public void Convert_DefaultContructorParametersNullableFalse_ReturnsUserFriendlyFalse()
+        {
+            bool? nullBool = false;
+            var result = new UserFriendlyBool().Convert(nullBool);
+
+            Assert.Equal(UserFriendlyBool.UserFriendlyFalse, result.ToString());
+        }
+
+
+        [Fact]
+        public void Convert_ExplicitContructorParametersNullableFalse_ReturnsUserFriendlyFalse()
+        {
+            bool? nullBool = false;
+            var result = new UserFriendlyBool(altTrue, altFalse).Convert(nullBool);
+
+            Assert.Equal(altFalse, result.ToString());
+        }
+
+        [Fact]
+        public void Convert_ExplicitContructorParametersWithBool_ReturnsUserFriendlyValues()
+        {
+            Assert.Equal(
+                altFalse,
+                new UserFriendlyBool(altTrue, altFalse).Convert(false).ToString()
+            );
+            Assert.Equal(
+                altTrue,
+                new UserFriendlyBool(altTrue, altFalse).Convert(true).ToString()
+            );
+        }
+
+        [Fact]
+        public void Convert_ExplicitContructorParametersWithNullableBool_ReturnsUserFriendlyValues()
+        {
+            bool? nullBool = null;
+            bool? nullBoolFalse = false;
+            bool? nullBoolTrue = true;
+
+            Assert.Equal(
+                altFalse, 
+                new UserFriendlyBool(altTrue, altFalse).Convert(nullBool).ToString()
+            );
+            Assert.Equal(
+                altFalse, 
+                new UserFriendlyBool(altTrue, altFalse).Convert(nullBoolFalse).ToString()
+                
+            );
+            Assert.Equal(
+                altTrue, 
+                new UserFriendlyBool(altTrue, altFalse).Convert(nullBoolTrue).ToString()
+            );
+        }
+    }
+}
---
+++ src/kuujinbo.Mvc.NET/HtmlHelpers/UserFriendlyBool.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Mvc;
+
+namespace kuujinbo.Mvc.NET.HtmlHelpers
+{
+    /// <summary>
+    /// Strange, but true: some developers think it's OK to display true and
+    /// false in their UIs....
+    /// </summary>
+    public class UserFriendlyBool
+    {
+        public const string UserFriendlyTrue = "Yes";
+        public const string UserFriendlyFalse = "No";
+
+        public string TrueValue { get; private set; }
+        public string FalseValue { get; private set; }
+
+        /// <summary>
+        /// Convert boolean to a user-friendly dsiplayable value.
+        /// </summary>
+        public UserFriendlyBool(
+            string trueValue = UserFriendlyTrue, 
+            string falseValue = UserFriendlyFalse)
+        {
+            TrueValue = trueValue;
+            FalseValue = falseValue;
+        }
+
+        /// <summary>
+        /// Convert boolean to a human-readable valiu
+        /// </summary>
+        public MvcHtmlString Convert(bool value)
+        {
+            return new MvcHtmlString(value ? TrueValue : FalseValue);
+        }
+
+        /// <summary>
+        /// Convert boolean to a human-readable valiu
+        /// </summary>
+        public MvcHtmlString Convert(bool? value)
+        {
+            return new MvcHtmlString(value.HasValue && value.Value ? TrueValue : FalseValue);
+        }
+    }
+}
