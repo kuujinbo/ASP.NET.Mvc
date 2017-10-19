946d25c234dda33939f2d78b3acd6682281bc7e1
kuujinbo <kuujinbo@gmail.com>
2017-10-19 12:46:41 -0700

    BUG - AntiForgeryConfig.CookieName

--- src/kuujinbo.Mvc.NET/Attributes/ValidateJsonAntiForgeryTokenAttribute.cs
+++ src/kuujinbo.Mvc.NET/Attributes/ValidateJsonAntiForgeryTokenAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
 
@@ -21,10 +22,20 @@ namespace kuujinbo.Mvc.NET.Attributes
 
             var request = filterContext.HttpContext.Request;
             var cookie = request.Cookies[AntiForgeryConfig.CookieName];
+            /* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+             * System.Web.Helpers.AntiForgeryConfig.GetAntiForgeryCookieName()
+             * (System.Web.WebPages.dll) appends garbage to the cookie name 
+             * that is dependent on the web application root.
+             * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            */
             string cookieToken = cookie != null ? cookie.Value : null;
 
             AntiForgery.Validate(
-                cookieToken, request.Headers[AntiForgeryConfig.CookieName]
+                cookieToken, request.Headers["__RequestVerificationToken"]
+                //                            ^^^^^^^^^^^^^^^^^^^^^^^^^^    
+                // HtmlHelper.AntiForgeryToken() **ALYWAS** sets the
+                // hidden form field name to above; would be nice if M$ 
+                // gave programmatic access so no hard-coded value....
             );
         }
     }
