abcf5433caeb28e98a58a77859e3c3fb7d77e004
kuujinbo <kuujinbo@gmail.com>
2018-01-23 10:54:33 -0800

    EnumExtension

---
+++ src/kuujinbo.Mvc.NET.Tests/Helpers/EnumExtensionTests.cs
@@ -0,0 +1,30 @@
+﻿using kuujinbo.Mvc.NET.Helpers;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace kuujinbo.Mvc.NET.Tests.Helpers
+{
+    public class EnumExtensionTests
+    {
+        const string AttributeOne = "Attribute One";
+        const string AttributeTwo = "Attribute Two";
+
+        enum _testEnum
+        {
+            [DisplayFormat(DataFormatString = AttributeOne)]
+            One,
+            [DisplayFormat(DataFormatString = AttributeTwo)]
+            Two
+        }
+
+        [Fact]
+        public void GetAttributeValue_ValidEnum_ReturnsValue()
+        {
+            Assert.Equal(
+                AttributeTwo,
+                _testEnum.Two.GetAttributeValue<DisplayFormatAttribute>()
+                    .DataFormatString
+            );
+        }
+    }
+}
---
+++ src/kuujinbo.Mvc.NET/Helpers/EnumExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace kuujinbo.Mvc.NET.Helpers
+{
+    public static class EnumExtension
+    {
+        /// <summary>
+        /// Get the custom attribute value associated to an enum.
+        /// </summary>
+        public static TAttribute GetAttributeValue<TAttribute>(this Enum value)
+            where TAttribute : Attribute
+        {
+            return value.GetType()
+                .GetMember(value.ToString())
+                .First()
+                .GetCustomAttribute<TAttribute>();
+        }
+    }
+}
